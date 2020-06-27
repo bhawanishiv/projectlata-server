fb7fb8f06404552ae83e5a12a1a2334205a2f8ec
Bhawani Shankar Bharti <bhawanishiv@gmail.com>
2020-06-28 03:17:19 +0530

    rechanged port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const config = require('config');
 const http = require('http');
 const app = require('./app');
-const port = config.get('PORT') || 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
