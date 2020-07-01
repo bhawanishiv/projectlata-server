5f5eb70e8d14a000f710f74b4a8123a67b75d5e3
Bhawani Shankar Bharti <bhawanishiv@gmail.com>
2020-07-01 19:10:04 +0530

    dev middleware json() changed to send()

--- api/middleware/dev.js
+++ api/middleware/dev.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
     const { instanceId } = req.body;
     if (!instanceId) return res.status(404).send('error:no-instanceId');
     db.ref(`instances/${instanceId}`).once('value', (snapshot) => {
-        if (!snapshot.val()) return res.status(404).json('error:invalid-instance');
+        if (!snapshot.val()) return res.status(404).send('error:invalid-instance');
         req.instance = snapshot;
         next();
     })
