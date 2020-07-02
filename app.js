cbdd14ace311122a279a41ef1847b6a79abfa243
Bhawani Shankar Bharti <bhawanishiv@gmail.com>
2020-07-03 04:21:00 +0530

    added body-parser back

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const config = require('config');
 const cors = require('cors');
 const admin = require('firebase-admin');
 
 app.use(cors());
 app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 const serviceAccount = require('./project-lata-firebase-adminsdk-t3zqd-bf2be61d49.json');
